7cef700a84e16ff55f80c2f08ef4cc40a92edb31
glata95 <galtaebi@gmail.com>
2020-04-12 18:27:22 +0300

    Added axios and get request to insert/update data

---
+++ src/utils/requests.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { baseSqlApiRoute, API_KEY } from '../constants/routes';
+
+export async function insertUpdate(sql) {
+    const params = {
+        q: sql,
+        api_key: API_KEY
+    }
+
+    axios.get(`${baseSqlApiRoute}`, { params }).then(res => {
+        return  res.data
+    }).catch (e => {
+        return e;
+    })
+}
