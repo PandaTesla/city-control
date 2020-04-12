95f1bf5eb446aba4a212041bb2f38af1f0533960
Ran Elishayev <42379604+rannyeli@users.noreply.github.com>
2020-04-12 18:50:21 +0300

    feat(form): added update form and small changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import {AppBar, Toolbar, Typography} from '@material-ui/core';
+import React,{useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {AppBar, Toolbar, Typography, Tabs, Tab} from '@material-ui/core';
 import './App.css';
 
 import Form from './components/Form'
 
+const useStyles = makeStyles({
+  tabs: {
+      margin: '0 auto',
+  },
+});
 
 function App() {
+  const classes = useStyles();
+  const [tabNum, setTabNum] = useState(0);
+
+  const handleTabChange = (event, newTabNum) => {
+    setTabNum(newTabNum)
+  }
+
   return (
     <>
       <AppBar position="static">
@@ -13,9 +26,13 @@ function App() {
           <Typography variant="h6">
             City Control
           </Typography>
+          <Tabs value={tabNum} onChange={handleTabChange} className={classes.tabs}>
+            <Tab label="צור משימה חדשה"/>
+            <Tab label="ערוך משימה קיימת"/>
+          </Tabs>
         </Toolbar>
       </AppBar>
-      <Form/>
+      <Form type={tabNum}/>
     </>
   );
 }
