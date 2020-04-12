4bc77f089c6f6864cd814378cbd8dd0d282a5936
Gal Taebi <galtaebi@gmail.com>
2020-04-12 21:07:23 +0300

    Add get by id from sql api

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,5 @@
 export const API_KEY = "ca6b4cc11768c8d258787485a5c917fe2853bf30";
 
 const userName = "elbit";
+export const tableName = "elbit.wide_bridge_copy"
 export const baseSqlApiRoute = `https://cartodb.covid-97.com/user/${userName}/api/v2/sql/`;
--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,18 +1,36 @@
 import axios from 'axios';
-import { baseSqlApiRoute, API_KEY } from '../constants/routes';
+import { baseSqlApiRoute, API_KEY, tableName } from '../constants/routes';
+
+const headers =  {
+    'Access-Control-Allow-Origin': '*'
+}
 
 export async function insertUpdate(sql) {
     const params = {
         q: sql,
         api_key: API_KEY,
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        }
+        headers: headers
     }
 
     axios.get(`${baseSqlApiRoute}`, { params }).then(res => {
         return res.data
     }).catch (e => {
         return e;
+    })
+}
+
+export async function getByID(id) {
+    const sql = `SELECT * from ${tableName} WHERE cartodb_id=${id}`
+
+    const params = {
+        q: sql,
+        api_key: API_KEY,
+        headers: headers
+    }
+
+    axios.get(`${baseSqlApiRoute}`, { params }).then(res => {
+        console.log(res.data)
+    }).catch (e => {
+        console.log(e);
     })
 }
