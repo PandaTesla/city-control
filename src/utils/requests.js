2f813391c5c6deb0bb4628b11c2995ce88bf43f9
Ran Elishayev <42379604+rannyeli@users.noreply.github.com>
2020-04-13 16:17:31 +0300

    feat(notifier): notify on save

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -15,14 +15,26 @@ export async function insertUpdate(sql) {
     params.q = sql
 
     return axios.get(`${sqlApi.url}`, { params })
-    .then(res => res.data)
-    .catch (e => e);
+    .then(res => ({
+        status: res.status,
+        data: res.data
+    }))
+    .catch (e => ({
+        status: e.response.status,
+        data: e.response.data
+    }));
 }
 
 export async function getByID(id) {
     params.q = `SELECT * from ${sqlApi.tableName} WHERE cartodb_id=${id}`
 
     return axios.get(`${sqlApi.url}`, { params })
-    .then(res => res.data.rows[0])
-    .catch (e => e);
+    .then(res => ({
+        status: res.status,
+        data: res.data.rows[0]
+    }))
+    .catch (e => ({
+        status: e.response.status,
+        data: e.response.data
+    }));
 }
