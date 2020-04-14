15f25ce41083358d7e12c1e9aa36230b529b19b5
Gal Taebi <galtaebi@gmail.com>
2020-04-14 14:34:51 +0300

    feat(requests): updating multiple features

--- src/utils/converter.js
+++ src/utils/converter.js
@@ -1,75 +1,71 @@
 import {SQL_API} from '../constants/routes'
-const tableName = SQL_API.tableName;
+const tableName = SQL_API.tableNameForUpateInsert;
 
-export default function converter(input, type, cartoDbId) {
-    const tableItem = input;
-    var columns = [];
-    var columnTypes = [];
-    var columnInfo = [];
-    var values = [];
-    const valueInserts = [];
-  
-    parseObject(tableItem, type, cartoDbId);
-  
-    const sql = toSql(valueInserts);
-  
-    return sql;
-  
-    function parseObject(tableItem, type, cartoDbId) {
-      let query = '';
-  
-      convertObject(tableItem)
-      parseColumnInfo()
-      if (type === 'UPDATE') {
-        const set = createSet(columns, values)
-        query = `UPDATE ${tableName} SET ${set} WHERE cartodb_id = ${cartoDbId}`;
-      }
-      else {
-        query = `INSERT INTO ${tableName} (${columns}) VALUES (${values})`
-      }
-      valueInserts.push(query)
+export default function converter(inputArr, type) {
+  const tableItems = inputArr;
+  var columns = [];
+  var columnTypes = [];
+  var columnInfo = [];
+  var values = [];
+  const valueInserts = [];
+
+  tableItems.map((tableItem, i) => {
+    parseObject(tableItem, type, tableItem.cartodb_id);
+  })
+
+  return valueInserts;
+
+  function parseObject(tableItem, type, cartoDbId) {
+    let query = '';
+
+    convertObject(tableItem)
+    parseColumnInfo()
+    if (type === 'UPDATE') {
+      const set = createSet(columns, values)
+      query = `UPDATE ${tableName} SET ${set} WHERE cartodb_id = ${cartoDbId}`;
     }
-  
-    function convertObject(item) {
-      columns = [];
-      values = [];
-      for (var i in item) {
-        columns.push(i);
-        let value = item[i]
-        if (typeof value === 'string' && i !== "the_geom") {
-          value = "'" + value + "'";
-        }
-        if (value == null) {
-          value = 'NULL';
-        }
-        values.push(value);
+    else {
+      query = `INSERT INTO ${tableName} (${columns}) VALUES (${values})`
+    }
+    valueInserts.push(query)
+  }
+
+  function convertObject(item) {
+    columns = [];
+    values = [];
+    for (var i in item) {
+      columns.push(i);
+      let value = item[i]
+      if (typeof value === 'string' && i !== "the_geom") {
+        value = "'" + value + "'";
       }
-    }
-  
-    function parseColumnInfo() {
-      for (var i = 0; i < columns.length; i++) {
-        if (typeof (values[i]) == "string") {
-          columnTypes = "TEXT"
-          columnInfo.push(`${columns[i]} ${columnTypes}`)
-        }
-        else if (typeof (values[i]) == "number") {
-          columnTypes = "INTERGER"
-          columnInfo.push(`${columns[i]} ${columnTypes}`)
-        }
+      if (value == null) {
+        value = 'NULL'
       }
-    }
-   
-    function createSet(columns, values) {
-      let string = '';
-  
-      columns.forEach((col, i) => {
-        string += `${col} = ${values[i]}, `
-      });
-  
-      return string.slice(0, -2);
-    }
-  
-    function toSql(queries) {
-      return queries.join(`;\n`);
+      values.push(value);
     }
   }
+
+  function parseColumnInfo() {
+    for (var i = 0; i < columns.length; i++) {
+      if (typeof (values[i]) == "string") {
+        columnTypes = "TEXT"
+        columnInfo.push(`${columns[i]} ${columnTypes}`)
+      }
+      else if (typeof (values[i]) == "number") {
+        columnTypes = "INTERGER"
+        columnInfo.push(`${columns[i]} ${columnTypes}`)
+      }
+    }
+  }
+ 
+  function createSet(columns, values) {
+    let string = '';
+
+    columns.forEach((col, i) => {
+      string += `${col} = ${values[i]}, `
+    });
+
+    return string.slice(0, -2);
+  }
+}
--- src/utils/requests.js
+++ src/utils/requests.js
@@ -2,33 +2,34 @@ import axios from 'axios';
 import { AUTH, SQL_API, SQL_EXPRESSIONS } from '../constants/routes';
 
 let headers =  {
-    'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
 }
 
-let params = {
+let queryParams = {
     q: null,
     api_key: AUTH.apiKey,
-    headers: headers
 }
 
 export async function insertUpdate(sql) {
-    params.q = sql
+    const body = { query: sql };
+    const {q, ...queryPost} = queryParams
 
-    return axios.get(`${SQL_API.url}`, { params })
+    return axios.post(`${SQL_API.updateUrl}`, body, { params: queryPost, headers: headers })
     .then(res => ({
         status: res.status,
         data: res.data
     }))
     .catch(e => ({
-        status: e.response.status,
+        status: e,
         data: e.response.data
     }));
 }
 
 export async function getByColumn(columnName, value) {
-    params.q = SQL_EXPRESSIONS.selectByColumn + `${columnName} = '${value}'`
+    queryParams.q = SQL_EXPRESSIONS.selectByColumn + `${columnName} = '${value}'`
 
-    return axios.get(`${SQL_API.url}`, { params })
+    return axios.get(`${SQL_API.url}`, { params: queryParams })
     .then(res => ({
         status: res.status,
         data: res.data.rows
@@ -40,9 +41,9 @@ export async function getByColumn(columnName, value) {
 }
 
 export async function getColumnsName() {
-    params.q = SQL_EXPRESSIONS.selectColumnsNames
+    queryParams.q = SQL_EXPRESSIONS.selectColumnsNames
 
-    return axios.get(`${SQL_API.url}`, { params })
+    return axios.get(`${SQL_API.url}`, { params: queryParams })
     .then(res => ({
         status: res.status,
         data: res.data.rows.map(row => row.column_name).filter((columnName) => !['the_geom_webmercator', 'the_geom', 'lon', 'lat'].includes(columnName))
